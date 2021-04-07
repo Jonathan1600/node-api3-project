03799a61e8bbef6d86070ce0d257efeb5e393c59
unknown <jonathancalderonsilberman@gmail.com>
2021-04-07 16:10:22 -0700

    mvp done

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,51 @@
-function logger(req, res, next) {
+const Users = require("../users/users-model")
+
+const logger = (req, res, next) => {
   // DO YOUR MAGIC
+  console.log(req.method, req.path, Date.now())
+  next()
 }
 
-function validateUserId(req, res, next) {
+const validateUserId = async (req, res, next) => {
   // DO YOUR MAGIC
+  const { id } = req.params;
+  try {
+    const user = await Users.getById(id)
+    if (!user) {
+      res.status(404).json({ message: "user not found" })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (e) {
+    res.status(500).json(e.message)
+  }
 }
 
-function validateUser(req, res, next) {
+const validateUser = (req, res, next) => {
   // DO YOUR MAGIC
+  const body = req.body;
+  if (!body.name) {
+    res.status(400).json({ message: "missing required name field" })
+  } else {
+    next()
+  }
 }
 
-function validatePost(req, res, next) {
+const validatePost = (req, res, next) => {
   // DO YOUR MAGIC
+  const body = req.body;
+  if (!body.text) {
+    res.status(400).json({ message: "missing required text field" })
+  } else {
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validatePost,
+  validateUserId,
+  validateUser
+}
